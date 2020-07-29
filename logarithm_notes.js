05518cbee0e6e8b323d20b0096eddee61bca9665
Dave <dave.gagnat@gmail.com>
2020-07-29 11:30:00 -0500

    Started Logarithm notes

---
+++ logarithm_notes.js
@@ -0,0 +1,20 @@
+/*
+Logarithm: 
+
+  -> _ = 2 (In Computer Science, it is assume that it is always 2)
+
+  -> log_(x) = y iif (if and only if) 2^y = x
+
+  -> log_(N) = y iff 2^y = N
+
+
+  Just ask: 'What is 2^? = N. Said another way, as N doubles in size the ?
+  increases by only 1, so just a tiny amount.
+
+
+
+
+
+
+
+*/
