95cea967a0483a699c76b5fd8a5834a861d0519b
Dave <dave.gagnat@gmail.com>
2020-07-30 16:28:25 -0500

    Started Hash Table

---
+++ hash_tables_notes.js
@@ -0,0 +1,28 @@
+/*
+Hash Tables:
+  The slightly bad news is that Hash Tables are actually pretty complicated
+  under the hood or rather to make them very good and very performant, there is
+  a lot of complicated. Fortunately though, for the sake of coding interviews
+  we are dealing with the simple stuff. So, let us jump in. 
+
+  So whaat a Hash Table? To put it simply, a Hash Table is a key-value store. It
+  is a data structure where you are able to store pairs of keys and values. 
+  Where every key maps to a value. To be clear, you can access a value, given a
+  key. But, I would not be able to do the opposite.
+
+  Now keys do not have to be just integers like Arrays with indicies. Instead,
+  they can be strings, or other data types depending on the language we are 
+  coding in. And the beauty of a Hash Table is thata insertion of a key-value
+  pair, deletion of a key-value pair, or searching for a value given a key are 
+  all operations that run in constant time on average. 
+
+    -> insertion O(1) T
+    -> deletion O(1) T
+    -> searching O(1) T
+
+  At this point, you might be thinking, well, this weirdly sounds like an Array.
+
+
+
+
+*/
