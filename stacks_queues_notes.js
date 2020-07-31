38a7f92c060128cc1fab78aa0f172b6823101839
Dave <dave.gagnat@gmail.com>
2020-07-31 17:11:37 -0500

    Added Stacks & Queues

---
+++ stacks_queues_notes.js
@@ -0,0 +1,40 @@
+/*
+Stacks & Queues:
+  * Stack
+    It is a data structure that supports inserting and removing elements 
+    following the last in, first out principle. Which is why you may sometimes
+    hear people describe stacks as LIFO data structures. 
+
+      L-I-F-O (Last In, First Out)
+   
+      | ^
+      V |
+      ==== <- Last item in stack
+      ====
+      ==== <- Front of stack
+
+    The last element you put into your stack is the first element you are going
+    to take out of your stack.  
+      
+
+  * Queue
+    These are kind of the opposite of stacks. You can imagine the real life
+    example of a queue, as a queue or a line of people waiting to buy tickets
+    at the movie theater, for instance. And so, if you want to add an element to
+    a queue, you can imagine that someone new, would want to get in line to buy
+    tickets. Where would they go? The would go to the back of the line. 
+
+      O -> O O O O -> O 
+
+    And the first person who would get out of the line, would be the person who
+    first got into the line. They would buy their ticket, and then they would be
+    gone. This type of structure follows a different principle, it follows the
+    first in, first out principle.
+
+      F-I-F-O (First In, First Out)
+
+    The first person to get into the queue, is the first person that is going to
+    get out. Then the second person to get into the queue, is going to be the
+    next person to get out, and so on and so forth. 
+
+*/
