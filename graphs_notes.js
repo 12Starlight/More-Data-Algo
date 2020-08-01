74a2142d71e2cae6b4e59c027476a6058e22f111
Dave <dave.gagnat@gmail.com>
2020-08-01 15:43:25 -0500

    Added intro to Graphs

--- graphs_notes.js
+++ graphs_notes.js
@@ -1,7 +1,20 @@
 /*
 Graphs:
+  A Graph is a collection of nodes that may or may not be connected to one
+  another. Remember, in the Linked List section we called elements 'Nodes', for
+  Graphs it is the same thing. 
 
+  So, we have a bunch of Nodes that have integer values. So, for example, 1-11.
+  Now between these Nodes we have a bunch of arrows, and these arrows represent
+  the connections in between these Nodes. And at it's core, that is it. That is
+  a Graph. 
 
+  Now we actually give a name to the Nodes and the connections in a Graph. We
+  actually call the Nodes 'vertices', so every Node is a 'vertex' in the Graph,
+  and we call the connections or these arrows, we call these 'edges'. So, a 
+  Graph is really made up of two special things:
 
+    -> vertices (Nodes that might values, for instance integer values)
+    -> edges (connections that connect the nodes to one another)
 
 */
