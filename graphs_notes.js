0cbd37054397a44975d98f8836d5d9a22cc2ea50
Dave <dave.gagnat@gmail.com>
2020-08-01 16:03:12 -0500

    Added directed and undirected Graphs

--- graphs_notes.js
+++ graphs_notes.js
@@ -29,3 +29,36 @@ Graphs:
   that the Graph is NOT a connected Graph. 
 
 */
+
+/*
+  A second import concept in Graphs, is the fact that all of our edges are 
+  represented by arrows (in this case). And arrows imply direction. For 
+  instance, from Node 1, we might have an arrow that points to Node 2. We have
+  an arrow that points in one way, meaning we can only get to Node 2 from Node 
+  1, but we can not get to Node 1 from Node 2. There is only one direction that
+  this arrow goes in.
+
+  This is a very important concept in Graph theory. Basically the ideas is that
+  in some Graphs, this is one of those Graphs, are directed, meaning that the
+  edges in those Graphs have a direction. 
+
+  An undirected Graph would not have directions. So, for instance, if we were
+  dealing with an undirected Graph, then the edge between 1 and 2 would not be
+  an arrow. It would probably just be a straight line, and this would mean that
+  there is a connection between 1 and 2, but not in one direction.
+
+  The best way to conceptualize the difference between a directed Graph and an
+  undirected Graph is to think of real world examples. So when we are talking
+  about a directed Graph, you might imagine flights. If you have flights, flights
+  go from one airport to another airport. That would be a directed connection.
+  
+  On the other hand, let us think of friendships, especially Facebook 
+  friendships. If you are friends with someone on Facebook, then naturally, by
+  the definition of a Facebook friendship, that person is friends with you. So,
+  if they are your friend, you are their friend. 
+  
+  So, if we are talking about friendships on Facebook, then an undirected Graph 
+  might make more senses. There is no direction of the friendship, the 
+  friendship is a mutual connection.
+
+*/
