01fbb60eb5181d6b9ab9ca999836a5456a501da1
Dave <dave.gagnat@gmail.com>
2020-08-02 16:01:59 -0500

    Started twoNumberSum(array,targetSum)

--- problems/1_two_number_sum.js
+++ problems/1_two_number_sum.js
@@ -1,6 +1,6 @@
 /*
 Two Number Sum:
-  Write a function thata takes in a non-empty array of distinct integers and an
+  Write a function that takes in a non-empty array of distinct integers and an
   integer representing a target sum. If any two numbers in the input array sum
   up to the target sum, the function should return them in an array, in any
   order. If no two numbers sum up to the target sum, the function should return
