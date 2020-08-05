bc40a3d504f5b1dafcb17afb369e0ad06ea5cfed
Dave <dave.gagnat@gmail.com>
2020-08-05 12:56:38 -0500

    Added BST recursive version

--- problems/medium/7_bst_construction.js
+++ problems/medium/7_bst_construction.js
@@ -50,7 +50,7 @@ Sample Usage:
 
 // Average: O(log(N)) Time (N is the number of nodes in BST) | O(D) RecSpace (Where D is the height of the longest branch), IterSpace O(1)
 // Worst: O(N) Time | O(D) RecSpace (Where D is the height of the longest branch), IterSpace O(1)
-class BST {
+class BSTDemo {
   constructor(value) {
     this.value = value;
     this.left = null;
@@ -70,7 +70,7 @@ class BST {
 				// if currentNode.left === null
 				if (!currentNode.left) {
 					// effectively inserted value in our original BST on left
-					currentNode.left = new BST(value);
+					currentNode.left = new BSTDemo(value);
 					break;
 				} else {
 					// we still have a left subtree to explore
@@ -82,7 +82,7 @@ class BST {
 				// then check if the right subtree is null
 				if (!currentNode.right) {
 					// effectively inserted value in our original BST on right
-					currentNode.right = new BST(value);
+					currentNode.right = new BSTDemo(value);
 					break;
 				} else {
 					// we still have a right subtree to explore
@@ -195,5 +195,191 @@ class BST {
 		}
 		
 		return currentNode.value;
+	}
+}
+
+
+// BST Implemented without notes
+class BST {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+
+	insert(value) {
+		let currentNode = this;
+
+		while (true) {
+			if (value < currentNode.value) {
+				if (!currentNode.left) {
+					currentNode.left = new BST(value);
+					break;
+				} else {
+					currentNode = currentNode.left;
+				}
+			} else {
+				if (!currentNode.right) {
+					currentNode.right = new BST(value);
+					break;
+				} else {
+					currentNode = currentNode.right;
+				}
+			}
+		}
+
+		return this;
+	}
+
+	contains(value) {
+		let currentNode = this;
+
+		while (currentNode) {
+			if (value < currentNode.value) {
+				currentNode = currentNode.left;
+			} else if (value > currentNode.value) {
+				currentNode = currentNode.right;
+			} else {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	remove(value, parentNode = null) {
+		let currentNode = this;
+
+		while (currentNode) {
+			if (value < currentNode.value) {
+				parentNode = currentNode;
+				currentNode = currentNode.left;
+			} else if (value > currentNode.value) {
+				parentNode = currentNode;
+				currentNode = currentNode.right;
+			} else {
+				if (currentNode.left && currentNode.right) {
+					currentNode.value = currentNode.right.getMinValue();
+					currentNode.right.remove(currentNode.value, currentNode);
+				} else if (!parentNode) {
+					if (currentNode.left) {
+						currentNode.value = currentNode.left.value;
+						currentNode.right = currentNode.left.right;
+						currentNode.left = currentNode.left.left;
+					} else if (currentNode.right) {
+						currentNode.value = currentNode.right.value;
+						currentNode.left = currentNode.right.left;
+						currentNode.right = currentNode.right.right;
+					} else {
+						// this is a single node tree; do nothing.
+					}
+				} else if (parentNode.left === currentNode) {
+					parentNode.left = currentNode.left !== null ? currentNode.left : currentNode.right;
+				} else if (parentNode.right === currentNode) {
+					parentNode.right = currentNode.left !== null ? currentNode.left : currentNode.right;
+				}
+
+				break;
+			}
+		}
+
+		return this;
+	}
+
+	getMinValue() {
+		let currentNode = this;
+		while (currentNode.left) {
+			currentNode = currentNode.left;
+		}
+
+		return currentNode.value;
+	}
+}
+
+// BST Recursive Implemented Without Notes
+class BST {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+
+	insert(value) {
+		if (value < this.value) {
+			if (this.left === null) {
+				this.left = new BST(value);
+			} else {
+				this.left.insert(value);
+			}
+		} else {
+			if (this.right === null) {
+				this.right = new BST(value);
+			} else {
+				this.right.insert(value);
+			}
+		}
+
+		return this;
+	}
+
+	contains(value) {
+		if (value < this.value) {
+			if (this.left === null) {
+				return false;
+			} else {
+				return this.left.contains(value);
+			}
+		} else if (value > this.value) {
+			if (this.right === null) {
+				return false;
+			} else {
+				return this.right.contains(value);
+			}
+		} else {
+			return true;
+		}
+	}
+
+	remove(value, parent = null) {
+		if (value < this.value) {
+			if (this.left !== null) {
+				this.left.remove(value, this);
+			}
+		} else if (value > this.value) {
+			if (this.right !== null) {
+				this.right.remove(value, this);
+			}
+		} else {
+			if (this.left !== null && this.right !== null) {
+				this.value = this.right.getMinValueRec();
+				this.right.remove(this.value, this);
+			} else if (parent === null) {
+				if (this.left !== null) {
+					this.value = this.left.value;
+					this.right = this.left.right;
+					this.left = this.left.left;
+				} else if (this.right !== null) {
+					this.value = this.right.value;
+					this.left = this.right.left;
+					this.right = this.right.right;
+				} else {
+					// this is a single node tree; do nothing.
+				}
+			} else if (parent.left === this) {
+				parent.left = this.left !== null ? this.left : this.right;
+			} else if (parent.right === this) {
+				parent.right = this.left !== null ? this.left : this.right;
+			}
+		}
+
+		return this;
+	}
+
+	getMinValueRec() {
+		if (this.left === null) {
+			return this.value;
+		} else {
+			return this.left.getMinValueRec();
+		}
 	}
 }
