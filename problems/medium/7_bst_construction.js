845633c02961c19057a961c704810fe557a9eaa7
Dave <dave.gagnat@gmail.com>
2020-08-05 12:58:55 -0500

    Added BST recursive version

--- problems/medium/7_bst_construction.js
+++ problems/medium/7_bst_construction.js
@@ -200,6 +200,8 @@ class BSTDemo {
 
 
 // BST Implemented without notes
+// Average: O(log(N)) Time | O(log(N)) Space
+// Worst: O(N) Time | O(N) Space
 class BST {
 	constructor(value) {
 		this.value = value;
