f34fa1c6356f68f14527bebd311a54829224e989
Dave <dave.gagnat@gmail.com>
2020-08-06 13:21:01 -0500

    Solved findClosestValueInBst(tree,target) Iteratively and with correct understanding on my own

--- problems/easy/3_find_closest_value_in_bst.js
+++ problems/easy/3_find_closest_value_in_bst.js
@@ -81,7 +81,6 @@ const findClosestValueInBSTHelper = (tree, target, closest) => {
   return closest;
 }
 
-
 const findClosestValueInBst = (tree, target) => {
   return findClosestValueInBSTHelper(tree, target, Infinity);
 }
