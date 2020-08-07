02e080e5f62c3480ac2d3864aed2aa9cd8307c14
Dave <dave.gagnat@gmail.com>
2020-08-06 21:56:39 -0500

    Added depthFirstSearch(array)

---
+++ problems/easy/6_depth_first_search.js
@@ -0,0 +1,45 @@
+/*
+Depth First Search:
+  You are given a 'Node' class that has a 'name' and an array of optional
+  'children' nodes. When put together, nodes form an acyclic tree-like 
+  structure.
+
+  Implement the 'depthFirstSearch' method on the 'Node' class, which takes in an
+  empty array, traverses the tree using the Depth-first Search approach 
+  (specifically navigating the tree from left to right), stores all of the nodes'
+  nmaes in the input array, and returns it.
+
+  If you are unfamiliar with Depth-first Search, we recommend watching the
+  Conceptual Overview section of this question's video explanation before 
+  starting to code.
+
+Sample Input:
+      graph =    A
+              /  |  \
+             B   C   D
+           /   \   /   \
+          E     F G     H
+               / \ \
+              I   J K
+
+Sample Output:
+  ["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"]
+
+*/
+
+
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+    // Write your code here.
+  }
+}
