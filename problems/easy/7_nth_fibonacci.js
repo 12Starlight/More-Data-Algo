33a6f9738f184e074809f8b047f74d5b2e24c35c
Dave <dave.gagnat@gmail.com>
2020-08-09 17:08:02 -0500

    Solved getNthFib(n) iteratively, solved memo differently

--- problems/easy/7_nth_fibonacci.js
+++ problems/easy/7_nth_fibonacci.js
@@ -41,7 +41,15 @@ const getNthFibMemo = (n, memo = { 1: 0, 2: 1 }) => {
   return memo[n];
 }
 
-// O(N) Time | O(1) Space
+// Another Way To Do getNthFibMemo
+const getNthFibMemo = (n, memo = {}) {
+  if (n <= 1) return 0;
+  if (n === 2) return 1;
+  if (n in memo) return memo[n];
+  return memo[n] = getNthFib(n-1, memo) + getNthFib(n-2, memo);
+}
+
+// O(N) Time | O(1) Space (bc we are not storing anything)
 const getNthFib = (n) => {
   const lastTwo = [0, 1];
   let counter = 3;
@@ -56,6 +64,21 @@ const getNthFib = (n) => {
   return n > 1 ? lastTwo[1] : lastTwo[0];
 }
 
+// Iterative
+// O(N) Time | O(1) Space
+const getNthFibIter = (n) => {
+  let stack = [0,  1];
+  let counter = 3;
+
+  while (counter <= n) {
+    let nextCalc = stack[0] + stack[1];
+    stack[0] = stack[1];
+    stack[1] = nextCalc;
+    counter++;
+  }
+
+  return n > 1 ? stack[1] : stack[0];
+}
 
   
 
