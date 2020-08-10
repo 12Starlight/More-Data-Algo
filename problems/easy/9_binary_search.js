bad4bc5ce69b8b309e0ca07a6f7e773b81fa2310
Dave <dave.gagnat@gmail.com>
2020-08-09 21:42:52 -0500

    Added binarySearch(array, target)

---
+++ problems/easy/9_binary_search.js
@@ -0,0 +1,23 @@
+/*
+Binary Search:
+  Write a function that takes in a sorted array of integers as well as a target
+  integer. The function should use the Binary Search algorithm to determine if
+  the target integer is contained in the array and should return its index if it
+  is, otherwise '-1'.
+
+  If you are unfamiliar with Binary Search, we recommend the Conceptual Overview
+  section of this question's video explanation before starting to code.
+
+Sample Input:
+  array = [0, 1, 21, 33, 35, 45, 61, 71, 72, 73]
+  target = 33
+
+Sample Output:
+  3
+
+*/
+
+
+const binarySearch = (array, target) => {
+
+}
