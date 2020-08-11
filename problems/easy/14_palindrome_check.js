bb37dce560172d03a539d0737c32ff9234df00e2
Dave <dave.gagnat@gmail.com>
2020-08-11 16:14:25 -0500

    Added isPalindrome(string)

---
+++ problems/easy/14_palindrome_check.js
@@ -0,0 +1,20 @@
+/*
+Palindrome Check:
+  Write a function that takes in a non-empty string and that returns a boolean
+  representing whether the string is a palindorme.
+
+  A palindrome is defined as a string that is written the same forward and
+  backword. Note that single-character strings are palindromes.
+
+Sample Input:
+  string = 'abcdcba'
+
+Sample Output:
+  true // it is written the same forward and backward
+
+*/
+
+
+const isPalindrome = (string) => {
+  
+}
