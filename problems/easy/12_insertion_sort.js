e1c1631a474c09439826b7b61ae88c799a694879
Dave <dave.gagnat@gmail.com>
2020-08-11 15:05:59 -0500

    Soleved insertionSort(array) with helper

--- problems/easy/12_insertion_sort.js
+++ problems/easy/12_insertion_sort.js
@@ -17,5 +17,26 @@ Sample Output:
 
 
 const insertionSort = (array) => {
-  
+
+}
+
+
+// After explanation
+// O(N^2) Time | O(1) Space
+// Best: O(N) Time (sorted)
+const swap = (i, j, array) => {
+  [array[i], array[j]] = [array[j], array[i]];
+}
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+
+    let j = i
+    while (j > 0 && array[j] < array[j - 1]) {
+      swap(j, j - 1, array);
+      j--;
+    }
+  }
+
+  return array;
 }
