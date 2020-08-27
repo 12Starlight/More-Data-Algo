7a18716d76fb02adc83e31994642edaea1fd32a7
Dave <dave.gagnat@gmail.com>
2020-08-26 20:55:16 -0500

    Added getNthFib(n), Whiteboard Drills, Brute Force, 2nd time

--- problems/easy/7_nth_fibonacci.js
+++ problems/easy/7_nth_fibonacci.js
@@ -85,12 +85,20 @@ const getNthFibIter = (n) => {
 // 1: Brute Force: Recursion: N
 /// i: Base: N is two: Return one
 /// ii: Base: N is one: Return zero
-/// iii: Return: Recursion, n minus one: Plus: Recursion, n minus two
+/// iii: Return: Recursion, n minus one: Plus: Recursion, n minus two//
+// Avg: O(2^n) T (bc we call two frames n times) | O(n) S (bc n frames on call stack)
 const getNthFib = (n) => {
   if (n === 2) return 1;
   if (n === 1) return 0;
   return getNthFib(n - 1) + getNthFib(n - 2);
 }
 
-
-
+// 1: Brute Force: Recursion: N
+/// i: Base: N is two: Return one
+/// ii: Base: N is one: Return zero
+// Avg: O(2^n) T (bc we call two frames n times) | O(n) S (bc n frames on call stack)
+const getNthFib = (n) => {
+  if (n === 2) return 1;
+  if (n === 1) return 0;
+  return getNthFib(n - 1) + getNthFib(n - 2);
+}
