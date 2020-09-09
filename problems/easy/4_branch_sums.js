de4ca574fc917f17c1ce4d1098cf7e8a1e1e9690
Dave <dave.gagnat@gmail.com>
2020-09-09 17:39:07 -0500

    Added Whiteboard Drills, branchSums(root), Linear, Recursive, Optimal, 1st time

--- problems/easy/4_branch_sums.js
+++ problems/easy/4_branch_sums.js
@@ -32,6 +32,207 @@ Sample Output:
 */
 
 
+// class BinaryTree {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// // O(N) Time | O(N) Space
+// const calculateBranchSums = (node, runningSum, sums) => {
+//   if (!node) return;
+
+//   let newRunningSum = runningSum + node.value;
+//   if (!node.left && !node.right) {
+//     sums.push(newRunningSum);
+//     return;
+//   }
+
+//   calculateBranchSums(node.left, newRunningSum, sums);
+//   calculateBranchSums(node.right, newRunningSum, sums);
+// }
+
+// const branchSums = (root) => {
+//   let sums = [];
+//   calculateBranchSums(root, 0, sums);
+//   return sums;
+// }
+
+// // Solved on my own
+// // O(N) Time | O(N) Space
+// const calculateBranchSums = (node, runningSum, sums) => {
+//   if (!node) return;
+
+//   let newRunningSum = runningSum + node.value;
+//   if (!node.left && !node.right) {
+//     sums.push(newRunningSum);
+//   }
+
+//   calculateBranchSums(node.left, newRunningSum, sums);
+//   calculateBranchSums(node.right, newRunningSum, sums);
+// }
+
+// const branchSums = (root) => {
+//   let sums = [];
+//   calculateBranchSums(root, 0, sums);
+//   return sums;
+// }
+
+
+// // Whiteboard Drills
+// // 1a: Recursive: Optimal: HelperAParameters(node, ruunningSum, sums): class BinaryTree
+// /// i: Variable: sums: Return sums
+// // a: Calculate branch sums: Recursive: Base: Node is null: Return
+// /// i: All the node values on branch above current node value equal new running sum: Running sum plus node value
+// /// ii: Determine leaf nodes: Node left has zero chilren: Node right has zero children
+// /// iii: If at leaf node: Running sum is branch sum: Append running sum to sums list: Return
+// /// iiii: Not at leaf node: Recursive, calculate branch sums left: Recursive, calculate branch sums right
+// // Avg: O(log(n) T | O(n) S (bc n/2, half the tree nodes are leaf nodes which represent the sums list length)
+// const branchSums = (root) => {
+//   const sums = [];
+//   calculateBranchSumsHelper(root, 0, sums);
+//   return sums;
+// }
+
+// const calculateBranchSumsHelper = (node, runningSum, sums) => {
+//   if (!node) return; // if no children
+//   const newRunningSum = runningSum + node.value;
+
+//   if (!node.left && !node.right) { // at leaf
+//     sums.push(newRunningSum); // appending branch sum 
+//     return; // updates new sums list
+//   }
+
+//   calculateBranchSumsHelper(node.left, newRunningSum, sums);
+//   calculateBranchSumsHelper(node.right, newRunningSum, sums);
+// }
+
+// // class BinaryTree
+// class BinaryTree {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// // 1a: Recursive: Optimal: HelperAParameters(node, runningSum, sums): class BinaryTree
+// /// i: Variable: sums: Return sums
+// // a: Calculate branch sums: Recursive: Base: Node is null: Return
+// /// i: All the node values on branch above current node value equal new running sum: Running sum plus node value
+// /// ii: Determine leaf nodes: Node left has zero children: Node right has zero children
+// /// iii: If at leaf node: Running sum is branch sum: Append running sum to sums list: Return
+// /// iiii: Not at leaf node: Recursion, calculate branch sums left: Recursion, calculate branch sums right:
+// // Avg: O(n) T | O(n) S (bc n/2, half the tree nodes are leaf nodes which represent the sums list length)
+// const branchSums = (root) => {
+//   const sums = [];
+//   calculateBranchSums(root, 0, sums);
+//   return sums;
+// }
+
+// const calculateBranchSums = (node, runningSum, sums) => {
+//   if (!node) return;
+//   const newRunningSum = runningSum + node.value;
+
+//   if (!node.left && !node.right) {
+//     sums.push(newRunningSum);
+//   }
+
+//   calculateBranchSums(node.left, newRunningSum, sums);
+//   calculateBranchSums(node.right, newRunningSum, sums);
+// }
+
+// // class BinaryTree
+// class BinaryTree {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null; 
+//   }
+// }
+
+// // 1a: Recursive: Optimal: HelperAParameters(node, runningSum, sums): class BinaryTree
+// /// i: Variable: sums: Return sums
+// // a: Calculate branch sums: Recursive: Node is null: Return
+// /// i: All the node values on branch above current node value equal new running sum: Running sum plus node value
+// /// ii: Determine leaf nodes: Node left has zero children: Node right has zero children
+// /// iii: If at leaf node: Running sum is branch sum: Append running sum to sums list: Return
+// /// iiii: Not at leaf node: Recursive, calculate branch sums left: Recursive, calculate branch sums right
+// // Avg: O(n) T | O(n) S (bc n/2, half the tree nodes are leafs nodes which represent the sums list length)
+// const branchSums = (root) => {
+//   let sums = [];
+//   calculateBranchSums(root, 0, sums);
+//   return sums;
+// }
+
+// const calculateBranchSums = (node, runningSum, sums) => {
+//   if (!node) return;
+//   const newRunningSum = runningSum + node.value;
+
+//   if (!node.left && !node.right) {
+//     sums.push(newRunningSum);
+//   }
+
+//   calculateBranchSums(node.left, newRunningSum, sums);
+//   calculateBranchSums(node.right, newRunningSum, sums);
+// }
+
+// // class BinaryTree
+// class BinaryTree {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+
+const root = {
+  value: 1,
+  left: {
+    value: 2,
+    left: { value: 4, left: [], right: [] },
+    right: { value: 5, left: [], right: null }
+  },
+  right: {
+    value: 3,
+    left: { value: 6, left: null, right: null },
+    right: { value: 7, left: null, right: null }
+  }
+}
+
+
+/*
+Whiteboard Drills: Branch Sums: 1: 1a
+
+Explanation:
+Write a function that takes in a Binary Search Tree and returns a list of it's
+branch sums ordered from leftmost branch sum to rightmost branch sum.
+
+A branch sum is all the values of a BinaryTree branch. A BinaryTree branch is
+a path of nodes that starts from the root node and ends with any leaf node.
+
+Each BinaryTree node has an integer value, a left child node, and a right child
+node. Children nodes are either both BinaryTree nodes themselves or None / null.
+
+*/
+
+// Solution:
+// 1: Linear: Recursive: Optimal: 1sParameters(root): HelperAParameters(node, runningSum, sums): class BinaryTree
+/// i: Variable: sums: Assigned: Empty Array
+/// ii: Envoke aParameters(root, 0, sums)
+//// a: calculateBranchSums: Linear: Recursive: HelperAParameters(node, runningSum, sums)
+///// ai: Base: root is null: Return
+///// aii: Variable: newRunningSum: Assigned: runningSum plus node value: 
+/////      All the node values on branch above current node value is newRunningSum
+///// aiii: Conditional: Determine leaf nodes: node left has zero children and node right has zero children
+///// aiv: If at leaf node: runningSum is branch sum: Append newRunningSum to sums list: Return: Exit bc at leaf node
+///// av: Not at leaf node: Envoke: Recursion, aParameters(node left, newRunningSum, sums): Explore left tree:
+/////     Envoke: Recursion, aParameters(node right, newRunningSum, sums): Explore right tree
+/// iii: Return sums
+// Avg: O(n) T | O(n) S (bc n/2, half the tree nodes are leaf nodes which represent the sums list length)
 class BinaryTree {
   constructor(value) {
     this.value = value;
@@ -40,150 +241,21 @@ class BinaryTree {
   }
 }
 
-// O(N) Time | O(N) Space
-const calculateBranchSums = (node, runningSum, sums) => {
-  if (!node) return;
-
-  let newRunningSum = runningSum + node.value;
-  if (!node.left && !node.right) {
-    sums.push(newRunningSum);
-    return;
-  }
-
-  calculateBranchSums(node.left, newRunningSum, sums);
-  calculateBranchSums(node.right, newRunningSum, sums);
-}
-
-const branchSums = (root) => {
-  let sums = [];
-  calculateBranchSums(root, 0, sums);
-  return sums;
-}
-
-// Solved on my own
-// O(N) Time | O(N) Space
-const calculateBranchSums = (node, runningSum, sums) => {
-  if (!node) return;
-
-  let newRunningSum = runningSum + node.value;
-  if (!node.left && !node.right) {
-    sums.push(newRunningSum);
-  }
-
-  calculateBranchSums(node.left, newRunningSum, sums);
-  calculateBranchSums(node.right, newRunningSum, sums);
-}
-
-const branchSums = (root) => {
-  let sums = [];
-  calculateBranchSums(root, 0, sums);
-  return sums;
-}
-
-
-// Whiteboard Drills
-// 1a: Recursive: Optimal: HelperAParameters(node, ruunningSum, sums): class BinaryTree
-/// i: Variable: sums: Return sums
-// a: Calculate branch sums: Recursive: Base: Node is null: Return
-/// i: All the node values on branch above current node value equal new running sum: Running sum plus node value
-/// ii: Determine leaf nodes: Node left has zero chilren: Node right has zero children
-/// iii: If at leaf node: Running sum is branch sum: Append running sum to sums list: Return
-/// iiii: Not at leaf node: Recursive, calculate branch sums left: Recursive, calculate branch sums right
-// Avg: O(log(n) T | O(n) S (bc n/2, half the tree nodes are leaf nodes which represent the sums list length)
 const branchSums = (root) => {
   const sums = [];
-  calculateBranchSumsHelper(root, 0, sums);
+  calculateBranchSums(root, 0, sums);
   return sums;
 }
 
 const calculateBranchSumsHelper = (node, runningSum, sums) => {
-  if (!node) return; // if no children
-  const newRunningSum = runningSum + node.value;
-
-  if (!node.left && !node.right) { // at leaf
-    sums.push(newRunningSum); // appending branch sum 
-    return; // updates new sums list
-  }
-
-  calculateBranchSumsHelper(node.left, newRunningSum, sums);
-  calculateBranchSumsHelper(node.right, newRunningSum, sums);
-}
-
-// class BinaryTree
-class BinaryTree {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-// 1a: Recursive: Optimal: HelperAParameters(node, runningSum, sums): class BinaryTree
-/// i: Variable: sums: Return sums
-// a: Calculate branch sums: Recursive: Base: Node is null: Return
-/// i: All the node values on branch above current node value equal new running sum: Running sum plus node value
-/// ii: Determine leaf nodes: Node left has zero children: Node right has zero children
-/// iii: If at leaf node: Running sum is branch sum: Append running sum to sums list: Return
-/// iiii: Not at leaf node: Recursion, calculate branch sums left: Recursion, calculate branch sums right:
-// Avg: O(n) T | O(n) S (bc n/2, half the tree nodes are leaf nodes which represent the sums list length)
-const branchSums = (root) => {
-  const sums = [];
-  calculateBranchSums(root, 0, sums);
-  return sums;
-}
-
-const calculateBranchSums = (node, runningSum, sums) => {
   if (!node) return;
   const newRunningSum = runningSum + node.value;
 
   if (!node.left && !node.right) {
     sums.push(newRunningSum);
+    return; // Exit bc at leaf node
   }
 
-  calculateBranchSums(node.left, newRunningSum, sums);
-  calculateBranchSums(node.right, newRunningSum, sums);
-}
-
-// class BinaryTree
-class BinaryTree {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null; 
-  }
-}
-
-// 1a: Recursive: Optimal: HelperAParameters(node, runningSum, sums): class BinaryTree
-/// i: Variable: sums: Return sums
-// a: Calculate branch sums: Recursive: Node is null: Return
-/// i: All the node values on branch above current node value equal new running sum: Running sum plus node value
-/// ii: Determine leaf nodes: Node left has zero children: Node right has zero children
-/// iii: If at leaf node: Running sum is branch sum: Append running sum to sums list: Return
-/// iiii: Not at leaf node: Recursive, calculate branch sums left: Recursive, calculate branch sums right
-// Avg: O(n) T | O(n) S (bc n/2, half the tree nodes are leafs nodes which represent the sums list length)
-const branchSums = (root) => {
-  let sums = [];
-  calculateBranchSums(root, 0, sums);
-  return sums;
-}
-
-const calculateBranchSums = (node, runningSum, sums) => {
-  if (!node) return;
-  const newRunningSum = runningSum + node.value;
-
-  if (!node.left && !node.right) {
-    sums.push(newRunningSum);
-  }
-
-  calculateBranchSums(node.left, newRunningSum, sums);
-  calculateBranchSums(node.right, newRunningSum, sums);
-}
-
-// class BinaryTree
-class BinaryTree {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
+  calculateBranchSumsHelper(node.left, newRunningSum, sums); // Explore left tree
+  calculateBranchSumsHelper(node.right, newRunningSum, sums); // Explore right tree
 }
