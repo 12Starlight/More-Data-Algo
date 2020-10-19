f96c5d1e4c0832fdcb3e7ddc43ded25b847f60a7
Dave <dave.gagnat@gmail.com>
2020-10-19 01:42:02 -0500

    Wrote longestCommonSequenceOptimal(str1, str2), Optimal

--- problems/hard/9_longest_common_subsequence.js
+++ problems/hard/9_longest_common_subsequence.js
@@ -55,3 +55,43 @@ const longestCommonSubsequence = (str1, str2) => {
 }
 
 console.log(longestCommonSubsequence(str1, str2)) // ['x', 'y', 'z', 'w']
+
+// Optimal 4
+// O(nm) Time | O(nm) Space
+const longestCommonSubsequenceOptimal = (str1, str2) => {
+  const lengths = [];
+
+  for (let i = 0; i < str2.length + 1; i++) {
+    lengths.push(new Array(str1.length + 1).fill(0));
+  }
+
+  for (let i = 1; i < str2.length + 1; i++) {
+    for (let j = 1; j < str1.length + 1; j++) {
+      if (str2[i - 1] === str1[j - 1]) {
+        lengths[i][j] = lengths[i - 1][j - 1] + 1;
+      } else {
+        lengths[i][j] = Math.max(lengths[i - 1][j], lengths[i][j - 1]);
+      }
+    }
+  }
+  return buildSequence(lengths, str1);
+}
+
+const buildSequence = (lengths, string) => {
+  const sequence = [];
+  let i = lengths.length - 1;
+  let j = lengths[0].length - 1;
+
+  while (i !== 0 && j !== 0) {
+    if (lengths[i][j] === lengths[i - 1][j]) {
+      i--;
+    } else if (lengths[i][j] === lengths[i][j - 1]) {
+      j--;
+    } else {
+      sequence.unshift(string[j - 1]);
+      i--;
+      j--;
+    }
+  }
+  return sequence;
+}
