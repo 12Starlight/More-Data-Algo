4ae472253ff40980aa2686d2a6e058538135c966
Dave <dave.gagnat@gmail.com>
2021-09-14 17:59:25 -0500

    Added notes to Big O

--- notes/3_big_O_notation.js
+++ notes/3_big_O_notation.js
@@ -37,5 +37,47 @@ Different Asmyptotic Analysis:
   O(2^n) -> 2*2*2....2
   O(n!) -> n * (n-1) * (n-2) ...
 
+*/
+
+/* 
+Big O Notation
+  The notion used to describe the 'time complexity' and 'space complexity' of
+  algorithms.
+
+  Variables used in Big O notation denote the sizes of inputs to algorithms. For
+  example, 'O(n)' might be the time complexity of an algorithm that traverses
+  through an array of length 'n'; similarly, 'O(n + m) might be the time
+  complexity of an alogrithm that traverses through an array of length 'n' and
+  through a string of length 'm'.
+
+  The following are examples of common complexities and their Big O notations,
+  ordered from fastest to slowest:
+    * Constant : O(1)
+    * Logarithmic : O(log(n))
+    * Linear : O(n)
+    * Log-linear : O(nlog(n))
+    * Quadratic : O(n^2)
+    * Cubic : O(n^3)
+    * Exponential : O(2^n)
+    * Factorial : O(n!)
+
+
+Note that in the context of coding interviews, Big O notation is usually
+understood to describe the 'worst-case' complexity of an algorithm, even though
+the worst-case complexity might differ from the 'average-case' complexity.
+
+For example, some sorting algorithms have different time complexities
+depending on the layout of elements in their input array. In rare cases, their
+time complexity will be much worse than in more common cases. Similarly, an
+algorithm that takes in a string and performs special operations on uppercase
+characters might have a different time complexity when run on an input string of
+only uppercase characters vs. on an input string with just a few uppercase
+characters.
+
+Thus, when describing the time complexity of an algorithm, it can sometimes be
+helpufl to specify whether the time complexity refers to the average case or to
+the worse case (e.g., "this algorithm runs in O(nlog(n)) time on average and in
+O(n^2) time in the worst case").
+
 
 */
