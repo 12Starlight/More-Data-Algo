8305e1a2f43760cb1ef21ae146e5e79dcb476c25
Dave <dave.gagnat@gmail.com>
2021-10-13 14:14:26 -0500

    Watched Strings and added notes

--- notes/dsa_notes/9_strings.js
+++ notes/dsa_notes/9_strings.js
@@ -79,3 +79,23 @@ Strings:
   the string with value added. 
 
 */
+
+/* 
+String
+  One of the fundamental data types in Computer Science, strings are stored in
+  *memory* as *arrays* of integers, where each character in a given string is
+  mapped to an integer via some character-encoding standard like *ASCII*.
+
+  Strings behave much like normal arrays, with the main distinction being that,
+  in most programming languages (C++ is a notable exception), strings are
+  *immutable*, meaning that they can't be edited after creation. This also means
+  that simple operations like appending a character to a string are more
+  expensive than they might appear.
+
+  The operation above has a time complexity of *O(n^2)* where n is the length of
+  "string", because each addition of a character to "newString" creates an
+  entirely new string and is itself an *O(n)* operation. Therefore, n O(n)
+  operations are performed, leading to an O(n^2) time-complexity operation
+  overall.
+
+*/
