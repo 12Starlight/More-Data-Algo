e2d3a06c19b96455aa674acbbf1f5566f3ec1636
Dave <dave.gagnat@gmail.com>
2021-10-12 21:35:02 -0500

    Added Queue notes

--- notes/dsa_notes/8_stacks_queues.js
+++ notes/dsa_notes/8_stacks_queues.js
@@ -144,5 +144,23 @@ Stack
   linked list*.
 
 
+Queue
+  An array-like data structure whose elements follow the *FIFO* rule: *F*irst
+  *I*n, *F*irst *O*ut.
+
+  A queue is often compared to a group of people standing in line to purchase
+  items at a store: the first pserson to get in line is the first one to purchase
+  items and to get out of the queue.
+
+  The following are a queue's standard operations and their corresponding time
+  complexities:
+    * Enqueuing an element into the queue: O(1)
+    * Dequeuing an element out of the queue: O(1)
+    * Peeking at the element at the front of the queue: O(1)
+    * Searching for an element in the queue: O(n)
+  
+
+  A queue is typically implemented with a *doubly linked list*.
+
 
 */
