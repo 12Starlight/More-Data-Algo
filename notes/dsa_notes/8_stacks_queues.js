0508eb4989808b254701a6357d138c2724d51048
Dave <dave.gagnat@gmail.com>
2021-10-12 21:29:58 -0500

    Watched Stacks and Queues and added notes for stack

--- notes/dsa_notes/8_stacks_queues.js
+++ notes/dsa_notes/8_stacks_queues.js
@@ -123,3 +123,26 @@ Stacks & Queues:
   of the Stack, you peek at the next element in the Queue.
 
 */
+
+/*
+Stack
+  An array-like data structure whose elements follow the *LIFO* rule: *L*ast 
+  *I*n, *F*irst *O*ut.
+
+  A stack is often compared to a stack of books on a table: the last book that's
+  placed on the stack of books is the first one that's taken off the stack.
+
+  The following are a stack's standard operations and their corresponding time
+  complexities:
+    * Pushing an element onto the stack: O(1)
+    * Popping an element off the stack: O(1)
+    * Peeking at the element on the top of the stack: O(1)
+    * Searching for an element in the stack: O(n)
+
+
+  A stack is typically implemented with a *dynamic array* or with a *singly
+  linked list*.
+
+
+
+*/
