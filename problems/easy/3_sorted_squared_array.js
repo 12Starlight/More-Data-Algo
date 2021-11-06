04f12f832739b748feea1cf70f9576d45632d1c7
Dave <dave.gagnat@gmail.com>
2021-11-05 19:24:07 -0500

    Created prompt for Sorted Squared Array

---
+++ problems/easy/3_sorted_squared_array.js
@@ -0,0 +1,12 @@
+/*
+Sorted Squared Array
+
+Explanation:
+
+Write a function that takes in a non-emmpty array of integers that are sorted
+in ascending order and returns a new array of the same length with the squares
+of the original integers also sorted in ascending order.
+
+
+*/
+
