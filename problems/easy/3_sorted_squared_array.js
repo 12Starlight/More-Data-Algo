910bd2d0d69bf581aee98ed7cdf9aeef24196198
Dave <dave.gagnat@gmail.com>
2021-11-05 20:14:48 -0500

    Added Concept, Sudo, Edge Case for nlog(n)

--- problems/easy/3_sorted_squared_array.js
+++ problems/easy/3_sorted_squared_array.js
@@ -10,3 +10,31 @@ of the original integers also sorted in ascending order.
 
 */
 
+/*
+11/05/2021
+Time Complexity: O(nlog(n)) time : O(n) space
+
+
+Concept:
+
+Iterate through the array once and square each iterated number. Then push that
+squared value into a new array and return that array sorted.
+
+
+Sudo:
+
+Initalize an empty array.
+Iterate through the input array.
+  Square each iterated value.
+  Insert the squared value into the intialized array.
+
+Return the initialized array sorted.
+
+
+Edge Case:
+
+If no numbers are in the array, the empty array itself is returned.
+
+
+*/
+
