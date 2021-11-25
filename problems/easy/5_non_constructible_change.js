db28403b5a4780957e0f39d9259bf128f4222a89
Dave <dave.gagnat@gmail.com>
2021-11-25 13:33:00 -0600

    Added prompt to Non Constructible Change

---
+++ problems/easy/5_non_constructible_change.js
@@ -0,0 +1,24 @@
+/*
+Non Constructible Change
+
+Explanation:
+
+Given an array of positive integers representing the values of coins in your
+possession, write a function that returns the minimum amount of change (the
+minimum sum of money) that you CANNOT create. The given coins can have any 
+postive integer value and are not necessarily unique (i.e., you can have 
+multiple coins of the same value).
+
+For example, if you are given 'coins = [1, 2, 5], the minimum amount of change
+that you cannot create is '4'. If you are given no coins, the minimum amount of
+change that you cannot create '1'.
+
+
+Sample Input:
+  coins = [5, 7, 1, 1, 2, 3, 22]
+
+Sample Output:
+  20
+
+*/
+
