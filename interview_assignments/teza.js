00b488edff704c22a1affb6c601736001832367c
Dave <dave.gagnat@gmail.com>
2021-11-28 11:52:51 -0600

    Added Teza prompt

---
+++ interview_assignments/teza.js
@@ -0,0 +1,67 @@
+/*
+Teza Programming Assignment - Offsite
+
+Given a list of information about fills for different stocks, calculate the total
+profit or loss accrued for each symbol.
+
+The attached text file contains the following columns:
+  * time: Date and time of fill
+  * symbol: Symbol of product
+  * price: Price of fill
+  * buy_sell: Side of fill
+  * qty: Qty of fill
+
+
+Write a script in the language of your choice that calculates total PNL per
+symbol. You may ignore any trading costs (just calculate Gross PNL). The output
+should be a csv with just two columns: symbol, total_pnl, and one row per 
+symbol.
+
+Important: You may assume/confirm that positions are flat after totaling all buys
+and sells (both on a per-symbol basis, subsequently overall). In other words,
+you only need to worry about realized PNL - there should be no open positions
+with unrealized pnl left to mark to any particular price.
+
+
+For example, given the csv:
+
+time, symbol, buy_sell, price, qty
+7/1/2019 9:30:13 AM,A,B,1.0,100
+7/1/2019 9:30:15 AM,A,S,2.0,100
+7/1/2019 9:30:19 AM,B,S,3.0,1000
+7/1/2019 9:30:20 AM,B,B,4.0,1000
+7/1/2019 9:30:21 AM,B,B,5.0,100
+7/1/2019 9:30:23 AM,B,S,5.0,100
+
+
+Your output would be:
+symbol, total_pnl
+A, 100
+B, -1000
+
+
+Please return a copy of the source code and file with the output produced. Feel
+free to follow up with us, if there are any questions.
+
+
+Additional things to ask for more difficulty:
+
+The table should clearly display the following statistics for each symbol 
+traded:
+  * Product symbol
+  * Total valume traded
+  * Total gross P&L
+  * Statistics summarizing 'flat to flat' trades (see below for definition)
+    * Number of trades
+    * Percent Winners (count of winning trades/number of total trades)
+    * Median Trade P&L
+    * Average Trade P&L
+    * Max Trade P&L
+    * Min Trade P&L
+    * Median Trade length (in seconds)
+    * Average Trade length (in seconds)
+    * Max Trade length (in seconds)
+    * Min Trade length (in seconds)
+
+
+*/
